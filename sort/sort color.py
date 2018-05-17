fc3edebf431d39fd83c93894741add231201362d
biscuitsking <15637846726@163.com>
2018-05-17 19:37:27 +0800

    Create sort color.py

---
+++ sort/sort color.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        # 排序种类为三种时
+        def tripartion(nums, target):
+            
+            i = 0
+            j = 0
+            n = len(nums) - 1
+            
+            while j <= n:
+                if nums[j] < target:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                    j += 1
+                elif nums[j] > target:
+                    nums[j], nums[n] = nums[n], nums[j]
+                    n -= 1
+                else:
+                    j += 1
+        tripartion(nums, 1)
